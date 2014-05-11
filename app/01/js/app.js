7f484a8c5d12300e224dbe9cc9954cdab4e4f309
Emidio Croci <emidio.croci@gmail.com>
2014-05-11 07:13:36 +0200

    Modificati file per startup applicazione

--- app/01/js/app.js
+++ app/01/js/app.js
@@ -2,15 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('blog', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'blog.filters',
+  'blog.services',
+  'blog.directives',
+  'blog.controllers'
+]);
+/*.config(function($httpProvider){
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});*/
